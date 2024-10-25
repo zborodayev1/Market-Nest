366bda9717551071846bd7e94ed47fd217b6ea6b
zborodayev1 <zakharb713@gmail.com>
2024-10-25 23:44:28 +0500

    Up haeder

--- src/components/assets/Headers/Header.tsx
+++ src/components/assets/Headers/Header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
           </div> */}
           <Link to='/register'>
             <button className="w-28 h-10 bg-gradient-to-r from-[#173f35] to-[#14594c] text-white rounded-lg shadow-inner hover:from-[#14594c] hover:to-[#1a574a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#14594c] focus:from-[#14594c] focus:to-[#1a574a] dark:from-[#0e2b26] dark:to-[#113c34] dark:hover:from-[#113c34] dark:hover:to-[#14594c] dark:focus:from-[#113c34] dark:focus:to-[#14594c] transition-all ease-in-out duration-300 ">
-               Register
+              Register
             </button>
           </Link>
           <Link to='/login' className="mx-3">
